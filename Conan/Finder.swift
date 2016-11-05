1123e9d76ede8b045e5a2b333363ca9ca37963fb
danis <liaolei@arashivision.com>
2016-11-05 15:00:39 +0800

    添加基本的命令解析，准备开始finder的开发

---
+++ Conan/Finder.swift
@@ -0,0 +1,26 @@
+//
+//  Finder.swift
+//  Conan
+//
+//  Created by 黄明 on 2016/11/5.
+//  Copyright © 2016年 Danis. All rights reserved.
+//
+
+import Foundation
+
+class Finder {
+    
+    
+    /// 创建搜索Localized对象
+    ///
+    /// - Parameters:
+    ///   - path: 导入路径，文件或路径
+    ///   - output: 导出文件路径
+    init(path: String, output: String) throws {
+        
+    }
+    
+    func start() {
+        
+    }
+}
--- Conan/main.swift
+++ Conan/main.swift
@@ -10,3 +10,35 @@ import Foundation
 
 print("Hello, World!")
 
+let cmd1 = "find"
+let cmd2 = "check"
+
+let arguments = ProcessInfo.processInfo.arguments
+
+guard arguments.contains(cmd1) || arguments.contains(cmd2) else {
+    print("you must input find/check /path")
+    
+    exit(0)
+}
+
+if let findIndex = arguments.index(of: cmd1) {
+    var inputPath = ""
+    var outputPath = "~/conan_ouput"
+    
+    if findIndex < arguments.count - 1 {
+        inputPath = arguments[findIndex + 1]
+    }
+    if findIndex < arguments.count - 2 {
+        outputPath = arguments[findIndex + 2]
+    }
+    
+    do {
+        let finder = try Finder(path: inputPath, output: outputPath)
+        finder.start()
+        
+    } catch {
+        print(error)
+        
+        exit(0)
+    }
+}
